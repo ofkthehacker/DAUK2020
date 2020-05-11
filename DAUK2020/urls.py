2eae5cc9ba0026c0569930bf8c1b3d515430aff5
Omar Farooq Khan <ofkthehacker@gmail.com>
2020-05-11 20:37:34 +0500

    Second commit

--- DAUK2020/urls.py
+++ DAUK2020/urls.py
@@ -18,10 +18,10 @@ from django.conf.urls import url
 from django.views.static import serve
 from django.urls import path, include
 from django.contrib import admin
+from . import settings
 
 
 urlpatterns = [
-    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
